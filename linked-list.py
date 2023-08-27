4c7e0fd19502727802cd24a3fdafa82caa7cf893
Muhammad Ahsan <49687629+ahsan554@users.noreply.github.com>
2023-08-27 17:05:21 +0500

    python linked list example

---
+++ linked-list.py
@@ -0,0 +1,85 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Previous node is not in the list")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key):
+        current = self.head
+
+        if current and current.data == key:
+            self.head = current.next
+            current = None
+            return
+
+        prev = None
+        while current and current.data != key:
+            prev = current
+            current = current.next
+
+        if not current:
+            return
+
+        prev.next = current.next
+        current = None
+
+    def search(self, key):
+        current = self.head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+# Creating a linked list and adding elements
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.prepend(0)
+linked_list.insert_after_node(linked_list.head.next, 1.5)
+
+# Displaying the linked list
+linked_list.display()
+
+# Deleting a node
+linked_list.delete_node(1.5)
+
+# Searching for an element
+print("Search 2:", linked_list.search(2))
+print("Search 5:", linked_list.search(5))
